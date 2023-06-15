8e5814db9b92ecc494dd5dfdb823328d66f163b7
Nysander <kave7771@outlook.com>
2023-06-16 02:19:59 +0300

    feature: sending email with courses bug: it doesnt work

--- core/config.py
+++ core/config.py
@@ -4,12 +4,15 @@ from pydantic import BaseSettings, PostgresDsn
 
 
 class Config(BaseSettings):
-    # DB_URL: str = f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:" \
-    #                      f"{os.getenv('POSTGRES_PASSWORD')}@" \
-    #                      f"{os.getenv('POSTGRES_HOST')}:" \
-    #                      f"{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
     DB_URL: PostgresDsn
     JWT_SECRET_KEY: str = f"{os.getenv('JWT_SECRET_KEY')}"
     JWT_ALGORITHM: str = f"{os.getenv('JWT_ALGORITHM')}"
+    EMAIL_HOST: str = f"{os.getenv('EMAIL_HOST')}"
+    EMAIL_HOST_USER: str = f"{os.getenv('EMAIL_HOST_USER')}"
+    EMAIL_HOST_PASSWORD: str = f"{os.getenv('EMAIL_HOST_PASSWORD')}"
+    EMAIL_PORT: str = f"{os.getenv('EMAIL_PORT')}"
+    CELERY_BROKER_URL: str = f"{os.getenv('CELERY_BROKER_URL')}"
+    CELERY_BACKEND_URL: str = f"{os.getenv('CELERY_BACKEND_URL')}"
+
 
 config = Config()
